a3812686bbe8b36a5fa6e1039669c81d56c0e76c
Khaled Allam <kaaoaa505@gmail.com>
2025-06-15 17:08:02 +0300

    Generate custom class to control style font and colors

--- p02_analyzing_new_app/lib/main.dart
+++ p02_analyzing_new_app/lib/main.dart
@@ -1,5 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(title: 'just for test', home: Scaffold(body: Center(child: Text('Khaled Allam')))));
+  runApp(
+    MaterialApp(
+      title: 'just for test',
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 40, 63, 0),
+            Color.fromARGB(255, 0, 255, 225),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: const Center(
+        child: Text(
+          ('Khaled Allam \n' 'خالد علام'),
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
 }
