d96574fa0dc4871fd8dd27bc4d78f77efff1d963
Khaled Allam <kaaoaa505@gmail.com>
2025-07-09 10:29:19 +0300

    Add StartScreen widget with image and button for quiz app

--- p03_quiz_app/lib/start_screen.dart
+++ p03_quiz_app/lib/start_screen.dart
@@ -7,10 +7,21 @@ class StartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           const Text(
             'Start Screen',
             style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          Image.asset('assets/images/quiz.png', width: 100),
+          SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.redAccent,
+            ),
+            child: Text('Start'),
           ),
         ],
       ),
