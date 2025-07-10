df114ccadfe2f088e393e289917d0b8f53dfb968
Khaled Allam <kaaoaa505@gmail.com>
2025-07-10 14:40:37 +0300

    Refactor AnswerBtn to accept a callback function for improved interactivity

--- p03_quiz_app/lib/ui/buttons/answer_btn.dart
+++ p03_quiz_app/lib/ui/buttons/answer_btn.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AnswerBtn extends StatelessWidget {
-  AnswerBtn(this.txt, {super.key});
+  AnswerBtn(this.txt, this.fun, {super.key});
   String txt;
+  
+  void Function() fun;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: () {}, child: Text(txt));
+    return ElevatedButton(
+      onPressed: fun,
+      style: ElevatedButton.styleFrom(),
+      child: Text(txt),
+    );
   }
 }
