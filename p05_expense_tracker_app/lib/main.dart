e141738d900520e6d8d41e70fa5bc45a24dc1c48
Khaled Allam <kaaoaa505@gmail.com>
2025-07-24 11:18:09 +0300

    Adding Dark Mode

--- p05_expense_tracker_app/lib/main.dart
+++ p05_expense_tracker_app/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:expense_tracker_app/expenses/expenses_screen.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.blueGrey);
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 10, 100, 110),
+);
 
 void main() {
   runApp(const MyApp());
@@ -15,19 +19,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(useMaterial3: true).copyWith(
         colorScheme: kColorScheme,
+
         appBarTheme: AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
         ),
+
         cardTheme: CardThemeData().copyWith(
           color: kColorScheme.secondaryContainer,
           margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         ),
+
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             backgroundColor: kColorScheme.primaryContainer,
           ),
         ),
+
         textTheme: ThemeData().textTheme.copyWith(
           titleLarge: TextStyle(
             fontWeight: FontWeight.bold,
@@ -36,6 +44,25 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
+
+      darkTheme: ThemeData.dark(useMaterial3: true).copyWith(
+        colorScheme: kDarkColorScheme,
+
+        cardTheme: CardThemeData().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+
+      themeMode: ThemeMode.system,
+
       home: ExpensesScreen(),
     );
   }
