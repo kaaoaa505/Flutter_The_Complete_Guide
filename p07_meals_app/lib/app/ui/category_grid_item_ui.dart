65d6c05f4169be2731d4aa8d4934cca513ebda21
Khaled Allam <kaaoaa505@gmail.com>
2025-07-31 16:15:02 +0300

    CategoryGridItemUi border radius for item and on tap splash color

--- p07_meals_app/lib/app/ui/category_grid_item_ui.dart
+++ p07_meals_app/lib/app/ui/category_grid_item_ui.dart
@@ -13,19 +13,25 @@ class CategoryGridItemUi extends StatelessWidget {
       categoryModel.color.withAlpha(200),
     ];
 
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: colors,
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: colors,
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        categoryModel.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-          color: Theme.of(context).colorScheme.onSurface,
+        child: Text(
+          categoryModel.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
         ),
       ),
     );
