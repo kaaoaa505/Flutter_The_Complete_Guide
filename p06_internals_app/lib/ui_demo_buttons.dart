9f86ae914702394f439a0c8559535b8baaf184fc
Khaled Allam <kaaoaa505@gmail.com>
2025-07-31 09:30:51 +0300

    add const for UiDemoButtons constructor

--- p06_internals_app/lib/ui_demo_buttons.dart
+++ p06_internals_app/lib/ui_demo_buttons.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class UiDemoButtons extends StatefulWidget {
-  UiDemoButtons({super.key});
+  const UiDemoButtons({super.key});
 
   @override
   State<UiDemoButtons> createState() {
